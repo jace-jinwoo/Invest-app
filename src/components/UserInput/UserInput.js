d8315bed6a0058c86ae27327d3266b684237b9a6
jinwoo <jinwoo.jace@gmail.com>
2023-09-24 00:18:36 +0900

    [feat] Add css module

--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import classes from "./UserInput.module.css";
 
 const initialUserInput = {
   "current-savings": 10000,
@@ -29,8 +30,8 @@ const UserInput = (props) => {
   };
 
   return (
-    <form onClick={submitHandler} className="form">
-      <div className="input-group">
+    <form onClick={submitHandler} className={classes["form"]}>
+      <div className={classes["input-group"]}>
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
           <input
@@ -50,7 +51,7 @@ const UserInput = (props) => {
           />
         </p>
       </div>
-      <div className="input-group">
+      <div className={classes["input-group"]}>
         <p>
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
@@ -72,11 +73,15 @@ const UserInput = (props) => {
           />
         </p>
       </div>
-      <p className="actions">
-        <button onClick={resetHandler} type="reset" className="buttonAlt">
+      <p className={classes["actions"]}>
+        <button
+          onClick={resetHandler}
+          type="reset"
+          className={classes["buttonAlt"]}
+        >
           Reset
         </button>
-        <button type="submit" className="button">
+        <button type="submit" className={classes["button"]}>
           Calculate
         </button>
       </p>
